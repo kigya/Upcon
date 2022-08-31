bec501b9d94d4c989e3fb26b208e7eb950dd4b37
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-08-31 11:06:31 +0300

    Initial commit

---
+++ app/src/main/java/com/kigya/upcon/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,8 @@
+package com.kigya.upcon.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.kigya.upcon.R
+
+class ArticleFragment: Fragment(R.layout.fragment_article) {
+}
+
---
+++ app/src/main/java/com/kigya/upcon/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.kigya.upcon.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.kigya.upcon.R
+
+class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
+}
+
